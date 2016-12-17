e5b8a755a39c4ef9de772a4b6d9c1ae0832eca64
Nguyen Thien Thanh <nguyenthienthanh.218@gmail.com>
2016-12-18 00:37:04 +0700

    @Removed models folder

--- api/models/group-models.js
+++
@@ -1,5 +0,0 @@
-// import database from './connect-firebase';
-//
-// const key = database.ref().child('group').push().key;
-//
-// module.exports = database.ref(`/group/${key}`);
--- api/models/user-models.js
+++
@@ -1,6 +0,0 @@
-// import database from './connect-firebase';
-//
-// export default function NewModel(_key) {
-//   const key = _key || database.ref().child('users').push().key;
-//   return database.ref(`/users/${key}`);
-// }
