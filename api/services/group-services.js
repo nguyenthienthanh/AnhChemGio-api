76bca742e05f85f963c4a0e17d55e5dff7bfa58e
Nhật Tân Đặng <Nht Tn ng>
2017-01-04 17:43:51 +0700

    Complete Group

--- api/services/group-services.js
+++ api/services/group-services.js
@@ -11,16 +11,6 @@ export function getById(key) {
   });
 }
 
-export function create(group) {
-  return new Promise((resolve, reject) => {
-    const key = database().ref().child('conversations').push().key;
-    database().ref(`/conversations/${key}`)
-      .update(group)
-      .then(resolve({id: key, ...group}))
-      .catch(err => reject(err));
-  });
-}
-
 export function invite(groupId, userId) {
   return new Promise((resolve, reject) => {
     UserServices.getById(userId)
@@ -33,3 +23,14 @@ export function invite(groupId, userId) {
       .catch(err => reject(err));
   });
 }
+export function create(group) {
+  return new Promise((resolve, reject) => {
+    const key = database().ref().child('conversations').push().key;
+    database().ref(`/conversations/${key}`)
+      .update(group)
+      .then(resolve({id: key, ...group}))
+      .catch(err => reject(err));
+  });
+}
+
+
